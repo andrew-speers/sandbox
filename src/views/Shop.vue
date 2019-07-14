413d5c23e44fdac96dd66be6aeeedcb238f4456a
Andrew Speers <andrew.speers@gmail.com>
2019-07-14 14:30:08 -0400

    The Sock Update

---
+++ src/views/Shop.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="shop">
+    <h1>{{ product }}</h1>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+    data : function () {return {product: 'Socks'}}
+}
+</script>
+
+<style lang="scss">
+#shop {
+  color: red;
+}
+</style>
